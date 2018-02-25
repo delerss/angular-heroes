d85d7c9d3767ce2d48c3d945190fe202257834fa
Peter Horton <phorton@softstackfactory.org>
2018-02-25 00:03:47 +0000

    added click event and updated details template

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,12 +9,17 @@ import {HEROES} from '../mock-heroes';
 })
 
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  
+  /* hero: Hero = {
     id: 1,
     name:'Windstorm'
   
-  };
-  heroes= HEROES;
+  }; */
+  heroes = HEROES;
   constructor() { }
 
   ngOnInit() {
